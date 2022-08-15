156916bd526564da724ec1bd143f8f487b877816
joelcmk <57118300+joelcmk@users.noreply.github.com>
2022-08-15 15:52:12 -0500

    Add text to projects

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -7,10 +7,21 @@ function Projects() {
   return (
     <div className="projects flex flex-wrap justify-center">
       <div className="flex justify-center w-96 h-96 bg-gradient-to-tr from-pink-500 to-violet-500 m-3 rounded-lg">
+        <p>myFlix</p>
         <img className="w-60 h-60" src={Movies} alt="pokemon" />
       </div>
-      <div className="flex justify-center w-96 h-96 bg-gradient-to-tr from-pink-500 to-violet-500 m-3 rounded-lg">
-        <img className="w-60 h-60" src={Pokemon} alt="pokemon" />
+      <div className="w-96 h-10 bg-white"></div>
+      <div className="flex flex-col  w-96 h-96 bg-gradient-to-tr from-pink-500 to-violet-500 m-3 rounded-lg">
+        <p className="text-center mt-3 text-cyan-50 text-2xl">Pokemon</p>
+        <img className="w-60 h-60 my-0 mx-auto" src={Pokemon} alt="pokemon" />
+        <div className="text-center text-cyan-50 mb-11">
+          <a href="" className="m-3 underline pointer hover:no-underline">
+            Live App
+          </a>
+          <a href="" className="m-3 underline hover:no-underline">
+            Code
+          </a>
+        </div>
       </div>
       <div className="flex justify-center w-96 h-96 bg-gradient-to-tr from-pink-500 to-violet-500 m-3 rounded-lg">
         <img className="w-60 h-60" src={Chat} alt="pokemon" />
@@ -20,12 +31,6 @@ function Projects() {
       </div>
       <div className="flex justify-center w-96 h-96 bg-gradient-to-tr from-pink-500 to-violet-500 m-3 rounded-lg">
         <img className="w-60 h-60" src={Todo} alt="pokemon" />
-      </div>
-      <div className="flex justify-center w-96 h-96 bg-gradient-to-tr from-pink-500 to-violet-500 m-3 rounded-lg">
-        <img className="w-60 h-60" src={Chat} alt="pokemon" />
-      </div>
-      <div className="flex justify-center w-96 h-96 bg-gradient-to-tr from-pink-500 to-violet-500 m-3 rounded-lg">
-        <img className="w-60 h-60" src={Chat} alt="pokemon" />
       </div>
     </div>
   );
