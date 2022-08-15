2d88879a7d65481d2f134d00ad9d71e0bf77b38a
joelcmk <57118300+joelcmk@users.noreply.github.com>
2022-08-14 22:05:50 -0500

    Add footer

---
+++ src/Components/Footer.js
@@ -0,0 +1,10 @@
+function Footer() {
+  return (
+    <div>
+      <div className="h-px mx-4 mt-11 bg-slate-300"></div>
+      <div className="h-40 bg-003c3d"></div>
+    </div>
+  );
+}
+
+export default Footer;
--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,7 +1,7 @@
 function Projects() {
   return (
     <div className="projects flex flex-wrap justify-center">
-      <div className="w-96 h-96 bg-gradient-to-tr from-pink-500 to-violet-500 m-3 rounded-lg">
+      <div className="w-96 h-96 bg-transparent border-2 m-3 rounded-lg">
         Hello
       </div>
       <div className="w-96 h-96 bg-gradient-to-tr from-pink-500 to-violet-500 m-3 rounded-lg"></div>
