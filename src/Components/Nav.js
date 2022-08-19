23ff10935b3e2649de440714fd80be0cef467279
joelcmk <57118300+joelcmk@users.noreply.github.com>
2022-08-18 21:21:18 -0500

    Remove links

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,46 +1,10 @@
 function Nav() {
   return (
     <div>
-      <nav class="flex justify-between py-4 mb-11 bg-slate-100 space-x-5">
-        <p class="left-0 ml-3 text-2xl text-black font-bold">Joel Saucedo</p>
-        <div class="">
-          <a
-            href="/dashboard"
-            class="font-medium px-3 py-2 text-slate-700 underline hover:bg-slate-100 hover:text-slate-900 hover:no-underline"
-          >
-            Home
-          </a>
-          <a
-            href="/team"
-            class="font-medium px-3 py-2 text-slate-700 underline hover:bg-slate-100 hover:text-slate-900 hover:no-underline"
-          >
-            Projects
-          </a>
-          <a
-            href="/projects"
-            class="font-medium px-3 py-2  text-slate-700 underline hover:bg-slate-100 hover:text-slate-900 hover:no-underline"
-          >
-            Contact
-          </a>
-          <a
-            href="/reports"
-            class="font-medium px-3 py-2  text-slate-700 underline hover:bg-slate-100 hover:text-slate-900 hover:no-underline"
-          >
-            Blog
-          </a>
-          <a
-            href="/reports"
-            class="font-medium px-3 py-2  text-slate-700 underline hover:bg-slate-100 hover:text-slate-900 hover:no-underline"
-          >
-            About
-          </a>
-          <a
-            href="/reports"
-            class="mr-3 ml-3 px-3 py-2 rounded-full text-white bg-sky-500 hover: "
-          >
-            Resume
-          </a>
-        </div>
+      <nav class="flex justify-between py-4 mb-11">
+        <a class="left-0 ml-3 text-2xl text-slate-200 font-bold" href="">
+          Joel Saucedo
+        </a>
       </nav>
     </div>
   );
