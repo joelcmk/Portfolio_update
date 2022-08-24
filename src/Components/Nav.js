3d821164918a0c271f41a754b91598f07b7d086c
joelcmk <57118300+joelcmk@users.noreply.github.com>
2022-08-23 20:57:54 -0500

    Add social media links

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -5,6 +5,17 @@ function Nav() {
         <a class="left-0 ml-3 text-2xl text-black font-bold" href="/">
           Joel Saucedo
         </a>
+        <div className="flex text-blue-500 font-semibold">
+          <a href="https://www.linkedin.com/in/j-saucedo/" className="mx-5">
+            Linkedin
+          </a>
+          <a href="https://github.com/joelcmk" className="mx-5">
+            Github
+          </a>
+          <a href="#" className="mx-5">
+            Resume
+          </a>
+        </div>
       </nav>
     </div>
   );
