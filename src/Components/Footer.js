9caa03026794b93854303ffa63316c3a14506c8a
joelcmk <57118300+joelcmk@users.noreply.github.com>
2022-08-25 22:31:50 -0500

    Add contact form

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,6 +1,27 @@
 function Footer() {
   return (
-    <div>
+    <div className="mt-20">
+      <form className="max-w-lg my-0 p-3 mx-auto bg-purple-300 rounded-xl">
+        <div className="my-0 mx-auto w-11/12">
+          <label class="block">
+            <span class="block text-sm  font-medium text-slate-700">Email</span>
+            <input
+              type="email"
+              className="w-full pl-3 h-10 rounded-md peer ..."
+              placeholder="you@email.com"
+            />
+            <p class="mt-2 invisible peer-invalid:visible text-pink-600 text-sm">
+              Please provide a valid email address.
+            </p>
+          </label>
+
+          <label className="block">Your message:</label>
+          <textarea className="w-full px-3 h-40 rounded-md"></textarea>
+          <button className="px-6 py-2 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">
+            Submit
+          </button>
+        </div>
+      </form>
       <footer class="flex justify-between py-4 mt-11 mb-11">
         <a class="left-0 ml-3 text-2xl text-black font-bold" href="/">
           Joel Saucedo
