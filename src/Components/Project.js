6ecc5af02cf9b40dbca3fad29474ea17756fa1c1
joelcmk <57118300+joelcmk@users.noreply.github.com>
2022-09-08 21:51:52 -0500

    Fix project links

--- src/Components/Project.js
+++ src/Components/Project.js
@@ -2,24 +2,28 @@ function Project({ project, url, css }) {
   console.log(url);
   return (
     <div className="w-2/5 m-5 mb-20 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-105 duration-300">
-      <div
-        className={`flex justify-center items-center shadow-md ${project.background} mb-10 rounded-lg`}
-      >
-        <img className={css} src={project.projectImg} alt="pokemon" />
-      </div>
-
-      <diiv className="text-black font-sans">
-        <h3 className="text-4xl font-semibold">{project.projectName}</h3>
-        <p className="text-xl font-thin my-2 ">{project.projectDescription}</p>
-        <a
-          href={url}
-          target="_blank"
-          className="font-bold hover:underline"
-          rel="noreferrer"
+      <a href={url} target="_blank" rel="noreferrer">
+        <div
+          className={`flex justify-center items-center shadow-md ${project.background} mb-10 rounded-lg`}
         >
-          VIEW PROJECT
-        </a>
-      </diiv>
+          <img className={css} src={project.projectImg} alt="pokemon" />
+        </div>
+
+        <diiv className="text-black font-sans">
+          <h3 className="text-4xl font-semibold">{project.projectName}</h3>
+          <p className="text-xl font-thin my-2 ">
+            {project.projectDescription}
+          </p>
+          <a
+            href={url}
+            target="_blank"
+            className="font-bold hover:underline"
+            rel="noreferrer"
+          >
+            VIEW PROJECT
+          </a>
+        </diiv>
+      </a>
     </div>
   );
 }
