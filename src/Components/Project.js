2082393361c03afb77a4efddb950498cf1bdc94d
joelcmk <57118300+joelcmk@users.noreply.github.com>
2022-09-12 13:13:23 -0500

    Add live app link to projects

--- src/Components/Project.js
+++ src/Components/Project.js
@@ -1,19 +1,27 @@
-function Project({ project, url, css }) {
+function Project({ project, url, css, liveAppUrl }) {
   console.log(url);
   return (
     <div className="w-2/5 m-5 mb-20 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-105 duration-300">
-      <a href={url} target="_blank" rel="noreferrer">
+      <a href={liveAppUrl} target="_blank" rel="noreferrer">
         <div
           className={`flex justify-center items-center shadow-md ${project.background} mb-10 rounded-lg`}
         >
           <img className={css} src={project.projectImg} alt="pokemon" />
         </div>
 
-        <diiv className="text-black font-sans">
+        <diiv className="flex flex-col text-black font-sans">
           <h3 className="text-4xl font-semibold">{project.projectName}</h3>
           <p className="text-xl font-thin my-2 ">
             {project.projectDescription}
           </p>
+          <a
+            href={liveAppUrl}
+            target="_blank"
+            className="font-bold hover:underline"
+            rel="noreferrer"
+          >
+            LIVE APP
+          </a>
           <a
             href={url}
             target="_blank"
--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -13,6 +13,7 @@ function Projects() {
         'Pomodoro will help you to organize your time, so you can be productive and have free time to rest or play video games. ',
       background: 'bg-orange-200',
       url: 'https://github.com/joelcmk/Pomodoro',
+      liveAppUrl: 'https://joelcmk.github.io/Pomodoro',
       css: 'w-80 h-80',
     },
     Budget: {
@@ -22,6 +23,7 @@ function Projects() {
         'LoveDinero was made with love, it is a personal finance web application that will help you to stay on budget and attain your financial goals. Cancun here we go!',
       background: 'bg-green-200',
       url: 'https://github.com/joelcmk/budget-calculator',
+      liveAppUrl: 'https://joelcmk.github.io/budget-calculator',
       css: 'w-96 h-80',
     },
     Recipify: {
@@ -31,6 +33,7 @@ function Projects() {
         "Recipefy is a web app that will help you find thousands of food recipes. Relive old memories  cooking plates from your childhood or go wild and cook something that you've never had before. ",
       background: 'bg-sky-300',
       url: 'https://github.com/joelcmk/Recipefy',
+      liveAppUrl: 'https://joelcmk.github.io/Recipefy',
       css: 'w-80  h-80',
     },
     Meetup: {
@@ -40,6 +43,7 @@ function Projects() {
         'Find out the work that I did volunteering as a Full-stack Developer at Devlaunchers.',
       background: 'bg-purple-300',
       url: 'https://github.com/dev-launchers/platform__dl-ideas',
+      liveAppUrl: 'https://idea.devlaunchers.com',
       css: 'w-full h-80 rounded-md',
     },
   };
@@ -57,21 +61,25 @@ function Projects() {
         project={myProjects.Budget}
         url={myProjects.Budget.url}
         css={myProjects.Budget.css}
+        liveAppUrl={myProjects.Budget.liveAppUrl}
       />
       <Project
         project={myProjects.Pomodoro}
         url={myProjects.Pomodoro.url}
         css={myProjects.Pomodoro.css}
+        liveAppUrl={myProjects.Pomodoro.liveAppUrl}
       />
       <Project
         project={myProjects.Recipify}
         url={myProjects.Recipify.url}
         css={myProjects.Recipify.css}
+        liveAppUrl={myProjects.Recipify.liveAppUrl}
       />
       <Project
         project={myProjects.Meetup}
         url={myProjects.Meetup.url}
         css={myProjects.Meetup.css}
+        liveAppUrl={myProjects.Meetup.liveAppUrl}
       />
       <NextProject />
     </div>
