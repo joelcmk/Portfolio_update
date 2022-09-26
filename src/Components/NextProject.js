fbf9841876a86051d195c523cc2eea24820a0f79
joelcmk <57118300+joelcmk@users.noreply.github.com>
2022-09-26 12:17:54 -0500

    Improve responsive desing

--- src/Components/NextProject.js
+++ src/Components/NextProject.js
@@ -2,7 +2,7 @@ function NextProject() {
   return (
     <div className="w-full transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-105 duration-300 m-5">
       <div className="shadow-md my-0 mx-auto bg-slate-400 mb-10 rounded-lg h-96 flex justify-center items-center md:w-4/6 lg:w-4/6">
-        <h3 className="text-white text-7xl">COMING SOON</h3>
+        <h3 className="text-white text-xl md:text-7xl lg:text-7xl">COMING SOON</h3>
       </div>
 
       <diiv className="flex flex-col items-center text-black font-sans ">
